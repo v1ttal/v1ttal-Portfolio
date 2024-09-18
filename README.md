e78abb6847a6d42d94df677e554f948e06efde4c
M.Vittal Kumaar <Vittalkumaar0@gmail.com>
2024-09-18 19:56:39 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Portfolio
+
+A simple portfolio web site to showcase my work, built with Astro and TailwindCSS
---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Vittal | Personal Portfolio"
+export const SITE_DESCRIPTION =
+  "Welcome to My Professional Portfolio"
